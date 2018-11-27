8533c1e9a13eae1d2010def76e2da95f55d055d0
PesceJonathan <jonathanpesce@hotmail.com>
2018-11-26 21:32:47 -0500

    Add the basics of display the table At the moment the table is being dispalyed, but isn't styled properly to look good , meaning that still needs to be done.

--- Stockers/app/src/main/java/com/dawson/jonat/stockers/LoanCalculator/LoanCalculatorActivity.java
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/LoanCalculator/LoanCalculatorActivity.java
@@ -2,16 +2,23 @@ package com.dawson.jonat.stockers.LoanCalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dawson.jonat.stockers.R;
 
 public class LoanCalculatorActivity extends Activity {
 
-    Spinner spinner;
-    EditText amount, interestRate, minPayment;
+    Spinner spinnerYears;
+    EditText amountView, interestRateView, minPaymentView;
+    LinearLayout resultsSpace;
+    View resultsTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +29,54 @@ public class LoanCalculatorActivity extends Activity {
         this.fillYearSpinner();
     }
 
+    public void calculateAndDisplayResults(View view) {
+        try {
+            double amount = Double.parseDouble(this.amountView.getText().toString());
+            double interestRate = Double.parseDouble(this.interestRateView.getText().toString());
+            double minPayment = Double.parseDouble(this.minPaymentView.getText().toString());
+            int years = Integer.parseInt(spinnerYears.getSelectedItem().toString());
+
+            this.displayResults(LoanCalculator.calculateInterestPayoutSummary(amount, interestRate, minPayment, years));
+        } catch (Exception e) {
+            //Toast.makeText(this, "Please ensure that all the fields are filled in", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void displayResults(LoanPayoutSummary loanPayoutSummary) {
+        if (resultsTable == null) {
+            resultsTable = (LinearLayout)this.getLayoutInflater().inflate(R.layout.loan_calculator_result_table,resultsSpace);
+            ((TextView)findViewById(R.id.timeToPayOff)).setText(loanPayoutSummary.getTimeToPayOff() + "");
+            ((TextView)findViewById(R.id.originalBalance)).setText(loanPayoutSummary.getTimeToPayOff() + "");
+            ((TextView)findViewById(R.id.interestAccumulated)).setText(loanPayoutSummary.getTimeToPayOff() + "");
+            ((TextView)findViewById(R.id.totalPaid)).setText(loanPayoutSummary.getTimeToPayOff() + "fffff");
+            ((TextView)findViewById(R.id.amountLeft)).setText(loanPayoutSummary.getAmountLeftToPay() + "fffff");
+        }
+
+    }
+
     /**
      * Helper method that will initialize all of the views
      */
     private void initializeViews() {
-        this.spinner = (Spinner) findViewById(R.id.spinnerYears);
-        this.amount = (EditText) findViewById(R.id.amount);
-        this.interestRate = (EditText) findViewById(R.id.interestRate);
-        this.minPayment = (EditText) findViewById(R.id.minPayment);
+        this.spinnerYears = (Spinner) findViewById(R.id.spinnerYears);
+        this.amountView = (EditText) findViewById(R.id.amount);
+        this.interestRateView = (EditText) findViewById(R.id.interestRate);
+        this.minPaymentView = (EditText) findViewById(R.id.minPayment);
+        this.resultsSpace = findViewById(R.id.tableResults);
     }
 
+    /**
+     * Helper method that will add an array adapter to the spinner to represent the year options
+     */
     private void fillYearSpinner() {
         //Create the array adapter that will be userd for the spinner
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,
                 R.array.yearOptions, android.R.layout.simple_spinner_item);
 
-        //ff
+        //Set the drop down view
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        this.spinner.setAdapter(arrayAdapter);
+        this.spinnerYears.setAdapter(arrayAdapter);
     }
 }
