e450d1a252f71057e46540cdb0a364d83d97479d
dannymanzano172 <danny.manzano.172@gmail.com>
2018-11-28 16:09:05 -0500

    Added landscape layout and event handlers

--- Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
@@ -2,6 +2,7 @@ package com.dawson.jonat.stockers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,33 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void foreignExchangeClick(View v){
+
+    }
+
+    public void financialHintsClick(View v){
+
+    }
+
+    public void stockQuoteClick(View v){
+
+    }
+
+    public void notesClick(View v){
+
+    }
+
+    public void loanCalculatorClick(View v){
+
+    }
+
+    public void portfolioClick(View v){
+
+    }
+
+    public void messageClick(View v){
+
     }
 }
