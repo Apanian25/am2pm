579ecdecbeb5d631c5dc23600eec8fc163ef5d75
Lara Mo <laramo1999@gmail.com>
2018-11-28 23:23:28 -0500

    first final draft of about activity -> disable about option from about activity, display appropriate dialog depending on where the user clicks. TODO: solve scrolling issue. add implicit intent for option 2 in menu

--- Stockers/app/src/main/java/com/dawson/jonat/stockers/AboutActivity.java
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/AboutActivity.java
@@ -2,7 +2,11 @@ package com.dawson.jonat.stockers;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.dawson.jonat.stockers.Menu.Menus;
@@ -16,18 +20,58 @@ public class AboutActivity extends Menus {
     }
 
     /**
-     * Must override the onPause method - don't let the user call about activity from the about acitivty
+     * Must override the onPause method - the about activity will create/recreate
+     * an instance of it self when clicking on the about option.
+     * As soon as the user clicks back, the activity will close itself
      */
     @Override
     public void onPause(){
         super.onPause();
-
+        finish();
     }
 
-    public void showDialog(View view) {
+    /**
+     * Must override the option menu items - don't let the user click on the about option to launch
+     * the about activity from the about page itself
+     * @param true if successfully created
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        menu.getItem(0).setEnabled(false);
+        return true;
+    }
+    
+    public void showMoreInfo(View view) {
+        //declare a dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(R.string.dialog_string)
-                .setTitle(R.string.dialog_title);
-        builder.create().show();
+        builder.setTitle(R.string.dialog_title);
+
+        switch (view.getId()){
+            case R.id.john:
+                builder.setMessage(R.string.git_john);
+                break;
+            case R.id.lara:
+                builder.setMessage(R.string.git_lara);
+                break;
+
+            case R.id.nick:
+                builder.setMessage(R.string.git_nick);
+                break;
+            case R.id.danny:
+                builder.setMessage(R.string.git_danny);
+                break;
+        }
+
+        //close alert when user clicks ok
+        builder.setPositiveButton(R.string.btn_text, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 }
