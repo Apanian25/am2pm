2a056b93a9484b2fd71a3ab2bb6a021187020641
PesceJonathan <jonathanpesce@hotmail.com>
2018-11-30 01:06:36 -0500

    Add the receiving of the messages The problem is that it should be accepting the notification, but it hasn't, going to continue to test

--- Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
@@ -7,12 +7,23 @@ import android.view.View;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
-
-
 import com.dawson.jonat.stockers.Hints.FinancialHintsActivity;
 import com.dawson.jonat.stockers.Hints.HintsFragmentPagerAdapter;
+import com.dawson.jonat.stockers.Hints.Hints;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -25,6 +36,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,  new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                String newToken = instanceIdResult.getToken();
+                Log.d("newToken",newToken);
+            }
+        });
     }
 
     public void goToNoteActivity(View view) {
