ee9b78819cb4c5b63fd06d7b1b7e6d9a4ef27606
PesceJonathan <jonathanpesce@hotmail.com>
2018-11-29 23:53:06 -0500

    Set up the client app for the firebase messaging

---
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/Messaging/Messaging.java
@@ -0,0 +1,15 @@
+package com.dawson.jonat.stockers.Messaging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dawson.jonat.stockers.R;
+
+public class Messaging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+    }
+}
---
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/Messaging/StockersFirebaseMessagingService.java
@@ -0,0 +1,21 @@
+package com.dawson.jonat.stockers.Messaging;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class StockersFirebaseMessagingService extends FirebaseMessagingService {
+    private final String TAG = "MessagingService";
+
+
+    /**
+     *This method is called everytime the instance id token is updated.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.i(TAG, "Refresehed Token: " + token);
+        //Send the token to the server
+        //TODO sendRegistrationToServer(token);
+    }
+}
