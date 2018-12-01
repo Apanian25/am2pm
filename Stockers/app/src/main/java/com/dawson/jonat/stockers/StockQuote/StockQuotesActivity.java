2ec11734522f7026b4745224ff8c34f1cede53d3
Lara Mo <laramo1999@gmail.com>
2018-12-01 09:36:19 -0500

    add validaiton to stockQuote, can't contain more than 5

---
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/StockQuote/StockQuotesActivity.java
@@ -0,0 +1,49 @@
+package com.dawson.jonat.stockers.StockQuote;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.dawson.jonat.stockers.R;
+
+import java.util.ArrayList;
+
+public class StockQuotesActivity extends AppCompatActivity {
+    ArrayList<Ticker> list;
+    EditText ed;
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_stock_quotes);
+           list = new ArrayList<>();
+        //find edit text
+        ed = findViewById(R.id.tickerInput);
+    }
+
+        /**
+         * private helper method that generates an array list
+         */
+        private ArrayList<Ticker> getData() {
+            //will be adding to array if size is less than 5
+            if(list.size() < 5 ) {
+                list.add(new Ticker(ed.getText().toString(), R.drawable.about_back));
+                return list;
+            }
+            else {
+                Toast.makeText(this, R.string.more_than_five, Toast.LENGTH_SHORT).show();
+            }
+            return list;
+        }
+
+    public void addTicker(View view) {
+        RecyclerView rv = findViewById(R.id.tickerList);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new TickerAdapter(this, getData()));
+    }
+}
+
