87523685b91e91bdea77b3f1ad24231cbca56457
PesceJonathan <jonathanpesce@hotmail.com>
2018-12-02 15:58:09 -0500

    Add a view holder to be used by the news recycler view

---
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/Messaging/Articles/ViewHolder.java
@@ -0,0 +1,38 @@
+package com.dawson.jonat.stockers.Messaging.Articles;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.dawson.jonat.stockers.R;
+
+public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private static final String SUBJECT = "Credit Loan Calculation Results";
+    private Context contextOfView;
+
+    TextView textViewTitle;
+    TextView textViewDescription;
+
+    /**
+     * Users
+     *
+     * @param view
+     * @param context
+     */
+    public ViewHolder(View view, Context context) {
+        super(view);
+        textViewTitle = (TextView)view.findViewById(R.id.title);
+        textViewDescription = (TextView)view.findViewById(R.id.shortDescription);
+        view.setOnClickListener(this);
+    }
+
+    /**
+     * When an view holder is clicked, it will launch an intent to send an email
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        //Here the article link should be sent
+    }
+}
