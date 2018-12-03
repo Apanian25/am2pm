7a81da1f561e9acd2d48c3fd1948753fd793145f
JonathanPesce <jonathanpesce@hotmail.com>
2018-12-03 15:51:05 -0500

    Add most of the implementation for retrieving articles from firebase Most of the code is implemented, but not all the code is working. The authentication works, but for some reason, the onDataChange is not getting triggered, which is the reason the data is not being retrieved

--- Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dawson.jonat.stockers;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.support.annotation.NonNull;
@@ -12,11 +14,14 @@ import android.widget.Toast;
 import com.dawson.jonat.stockers.Hints.Hints;
 import com.dawson.jonat.stockers.Messaging.Messaging;
 import com.dawson.jonat.stockers.Messaging.NewsArticles;
+import com.dawson.jonat.stockers.Messaging.ArticlesRecyclerView.NewsRecyclerView;
+import com.dawson.jonat.stockers.Messaging.NewsArticlesActivity;
 import com.dawson.jonat.stockers.Messaging.NotificationUtilities;
 import com.dawson.jonat.stockers.Messaging.SubscriptionManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -32,19 +37,25 @@ public class MainActivity extends Activity {
 
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+    private Context context;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
         //Instantiate firebase auth
         mAuth = FirebaseAuth.getInstance();
 
         //Sign the user in with the predefined authentication identification
-        mAuth.signInAnonymously();
-
+        mAuth.signInAnonymously().addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(context, "This Worked", Toast.LENGTH_LONG).show();
+            }
+        });
 
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(this,  new OnSuccessListener<InstanceIdResult>() {
             @Override
