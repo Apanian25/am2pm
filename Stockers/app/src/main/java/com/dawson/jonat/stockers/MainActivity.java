c98f8eea7e1780eb342f0d2671910b85797f120a
dannymanzano172 <danny.manzano.172@gmail.com>
2018-12-04 18:54:09 -0500

    Fix minor issue with display setting on first visit

--- Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/MainActivity.java
@@ -1,24 +1,14 @@
 package com.dawson.jonat.stockers;
 
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.ContentResolver;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.ContactsContract;
-import android.widget.ListView;
-import com.dawson.jonat.stockers.ContactDialog.ContactInformation;
 import com.dawson.jonat.stockers.LoanCalculator.LoanCalculatorActivity;
-import java.io.InputStream;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.content.Context;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.dawson.jonat.stockers.Menu.SettingsActivity;
 import com.dawson.jonat.stockers.Messaging.NewsArticlesActivity;
@@ -45,31 +35,41 @@ public class MainActivity extends Menus {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
         //Check if first visit
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);;
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         String email = sp.getString("email", null);
         if(email == null){
             Intent intent = new Intent(this, SettingsActivity.class);
             launchActivity(intent);
         }
 
-        context = this;
+        TextView userTV = findViewById(R.id.usernameLbl);
+        userTV.setText(sp.getString("fname" , "") + " " + sp.getString("lname", null));
+
         //Instantiate firebase auth
         mAuth = FirebaseAuth.getInstance();
 
         //Sign the user in with the predefined authentication identification
-        mAuth.signInAnonymously().addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-            @Override
-            public void onSuccess(AuthResult authResult) {
-//                Toast.makeText(context, "This Worked", Toast.LENGTH_LONG).show();
-            }
-        });
-
+        mAuth.signInAnonymously();
+        //Subscribe to news service
         SubscriptionManager.sub("News", this, false);
 
 
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        //To set after setting has been saved
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        TextView userTV = findViewById(R.id.usernameLbl);
+        userTV.setText(sp.getString("fname" , "") + " " + sp.getString("lname", null));
+    }
+
+    //Start activities when the corresponding button is clicked
 
     public void foreignExchangeClick(View v) {
 
@@ -80,19 +80,15 @@ public class MainActivity extends Menus {
     }
 
     public void stockQuoteClick(View v) {
-        // what would usally be clicked
-        Intent intent = new Intent(MainActivity.this, StockQuotesActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(MainActivity.this, StockQuotesActivity.class));
     }
 
     public void notesClick(View v) {
-        Intent intent = new Intent(MainActivity.this, NoteActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(MainActivity.this, NoteActivity.class));
     }
 
     public void loanCalculatorClick(View v) {
-        Intent intent = new Intent(this, LoanCalculatorActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(this, LoanCalculatorActivity.class));
     }
 
     public void portfolioClick(View v) {
@@ -100,7 +96,6 @@ public class MainActivity extends Menus {
     }
 
     public void messageClick(View v){
-        Intent intent = new Intent(this, NewsArticlesActivity.class);
-        startActivity(intent);
+        startActivity(new Intent(this, NewsArticlesActivity.class));
     }
 }
