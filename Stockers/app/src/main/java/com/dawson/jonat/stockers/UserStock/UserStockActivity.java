b756e73c143f5375a3dc338d3ef49693f893524f
PesceJonathan <jonathanpesce@hotmail.com>
2018-12-05 22:44:54 -0500

    Add stock information class that will hold the information of the stocks taken from the php api

---
+++ Stockers/app/src/main/java/com/dawson/jonat/stockers/UserStock/UserStockActivity.java
@@ -0,0 +1,15 @@
+package com.dawson.jonat.stockers.UserStock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.dawson.jonat.stockers.R;
+
+public class UserStockActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_stock);
+    }
+}
